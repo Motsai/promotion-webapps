bb11020defb0b2359bd7e9ac64dbc94e89970a6c
JS Chenard <js.chenard@motsai.com>
2015-11-14 17:20:19 -0500

    Python script hack to get data from CySmart

---
+++ PythonScripts/Log2Bin.py
@@ -0,0 +1,30 @@
+import re
+import binascii
+
+filename = "Sitting_Standing_Sitting2_2Hz.log"
+
+f = open(filename, 'r')
+
+data = f.readlines()
+f.close()
+
+outfile = open(filename+'.bin', 'wb')
+
+pattern = '.*Value: \[(.*)\]'
+
+for line in data:
+    match = re.match(pattern, line)
+    if( match != None):
+        data = match.groups(0)
+        byteStrArr = data[0].split(':')
+        # StrBytes is a string representation of the bytes
+        StrBytes = ''.join(byteStrArr)
+        print StrBytes
+
+        # BinData is the binary format
+        BinData = binascii.unhexlify(StrBytes)
+        outfile.write(BinData)
+
+outfile.close()
+
+
